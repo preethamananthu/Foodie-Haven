beae8e91e6d619fb2978719692c21551d6f02930
preethamananthu <preethamananthu@gmail.com>
2023-07-28 18:01:02 +0530

    responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import {FaHamburger} from "react-icons/fa";
 function Header() {
   return (
     <div className="container mx-auto py-4 sm:px-12 md:px-24 flex justify-between text-2xl items-center border-b-2">
-        <h1 className="text-4xl font-bold">Foodie Haven</h1>
+        <h1 className="text-2xl lg:text-4xl font-bold">Foodie Haven</h1>
         <nav className="">
           <ul className="hidden lg:flex gap-20 text-neutral-500">
             <li><a href="" className="hover:text-neutral-800">Home</a></li>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,14 +2,14 @@ function Hero() {
   return (
     <div className="container mx-auto py-8 sm:px-12 md:px-24">
       <div className="lg:flex items-center justify-between">
-        <div className="text text-center lg:text-left">
+        <div className="text text-center lg:text-left ">
           <h1 className="md:text-8xl text-6xl font-serif mb-8">Welcome to Foodie Haven</h1>
           <p className="text-neutral-500 text-2xl mb-8">
             Your One-Stop Food Ordering Destination!
           </p>
           <a href="" className="py-2 px-4 bg-violet-500 rounded-lg text-2xl text-white">Explore</a>
         </div>
-        <img className="aspect-square rounded-full mx-auto mt-8 lg-mt-0" src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80" alt=""/>
+        <img className="aspect-square rounded-full mx-auto mt-8 lg:mt-0 lg:w-1/2" src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80" alt=""/>
       </div>
     </div>
   );
