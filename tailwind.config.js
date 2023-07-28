12a190c4c3376f7bc34c1579bed6f03e46ca4ffd
preethamananthu <preethamananthu@gmail.com>
2023-07-28 21:13:24 +0530

    icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,6 @@ export default {
     extend: {
       screens: { 'sm': { 'max': '768px' } },
     },
-    backgroundImage: {
-      'hero-bg': "url('https://images.unsplash.com/photo-1495567720989-cebdbdd97913?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80')",
-    },
   },
   plugins: [],
 }
