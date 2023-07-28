41a6b227d4b22a336e80f86e1018c0d7035875e8
preethamananthu <preethamananthu@gmail.com>
2023-07-28 17:34:38 +0530

    button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,8 +7,9 @@ function Hero() {
           <p className="text-neutral-500 text-2xl mb-8">
             Your One-Stop Food Ordering Destination!
           </p>
+          <a href="" className="py-2 px-4 bg-violet-500 rounded-lg text-2xl text-white">Explore</a>
         </div>
-        <img className="aspect-square rounded-full mx-auto" src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80" alt=""/>
+        <img className="aspect-square rounded-full mx-auto mt-8 lg-mt-0" src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80" alt=""/>
       </div>
     </div>
   );
