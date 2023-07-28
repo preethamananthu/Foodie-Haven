a6be62fab67143ff57a20312cc89a8a64fc37813
preethamananthu <preethamananthu@gmail.com>
2023-07-28 19:35:08 +0530

    ham menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,46 @@
-import {AiOutlineUser, AiOutlineShoppingCart} from "react-icons/ai";
-import {FaHamburger} from "react-icons/fa";
+import { useState } from "react";
+
+import Navlinks from "./Navlinks";
+
+import { AiOutlineUser, AiOutlineShoppingCart } from "react-icons/ai";
+import { FaHamburger } from "react-icons/fa";
 
 function Header() {
+const [open, setOpen] = useState(false);
+
   return (
     <div className="container mx-auto py-4 sm:px-12 md:px-24 flex justify-between text-2xl items-center border-b-2">
-        <h1 className="text-2xl lg:text-4xl font-bold">Foodie Haven</h1>
-        <nav className="">
-          <ul className="hidden lg:flex gap-20 text-neutral-500">
-            <li><a href="" className="hover:text-neutral-800">Home</a></li>
-            <li><a href="" className="hover:text-neutral-800">Menu</a></li>
-            <li><a href="" className="hover:text-neutral-800">About us</a></li>
-          </ul>
-        </nav>
+      <h1 className="text-2xl lg:text-4xl font-bold">Foodie Haven</h1>
+      <nav className="hidden lg:block">
+        <Navlinks />
+      </nav>
 
-        <div className='lg:hidden'><FaHamburger /></div>
-          <ul className="hidden lg:flex gap-8 text-neutral-500">
-            <li><a href="" className="hover:text-neutral-800"><AiOutlineShoppingCart/></a></li>
-            <li><a href="" className="hover:text-neutral-800"><AiOutlineUser/></a></li>
-          </ul>
+      <div className="lg:hidden">
+        <button className="relative"
+        onClick={()=> {setOpen(!open)
+          console.log(open);
+         }}
+        >
+          <FaHamburger />
+        </button>
+        
+          
+        {open && <Navlinks />}
       </div>
-  )
+      <ul className="hidden lg:flex gap-8 text-neutral-500">
+        <li>
+          <a href="" className="hover:text-neutral-800">
+            <AiOutlineShoppingCart />
+          </a>
+        </li>
+        <li>
+          <a href="" className="hover:text-neutral-800">
+            <AiOutlineUser />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
---
+++ src/components/Navlinks.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+
+import { RxCross1 } from "react-icons/rx";
+
+function Navlinks() {
+    const [open, setOpen] = useState(false);
+  return (
+    <div>
+        <div className="
+        absolute w-full h-full top-0 left-0 bg-slate-200 bg-opacity-[98%]
+        lg:static lg:w-fit lg:bg-opacity-0
+        ">
+        <ul className="
+            flex flex-col items-center font-semibold h-screen justify-center text-4xl
+            gap-20 text-neutral-800 relative
+            lg:flex-row lg:font-normal lg:h-full lg:text-neutral-500 lg:text-2xl
+        ">
+<button className="absolute top-[6vh] right-[6vh] lg:hidden text-neutral-800 text-4xl z-10" 
+        onClick={()=>{
+          setOpen(!open)
+          console.log(open)
+        }}><RxCross1 /></button>
+        
+
+          <li>
+            <a href="" className="hover:text-neutral-800">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:text-neutral-800">
+              Menu
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:text-neutral-800">
+              About us
+            </a>
+          </li>
+        </ul>
+        </div>
+       
+    </div>
+  )
+}
+
+export default Navlinks
