9a56b90e6b3ecfd465b27fbc663a9bc1796560cc
preethamananthu <preethamananthu@gmail.com>
2023-07-28 23:27:43 +0530

    added menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./components/Header.tsx";
 import Hero from "./components/Hero.tsx";
-
+import Popular from "./components/Popular.tsx";
 
 function App() {
   return (
     <div className="h-screen bg-cover bg-no-repeat bg-[url('')] ">
       <Header />
       <Hero />
+      <Popular />
     </div>
   );
 }
---
+++ src/components/Popular.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+function Popular() {
+  const [popular, setPopular] = useState([]);
+
+  async function getData() {
+    const response = await fetch(
+      `https://api.spoonacular.com/recipes/random?number=6&apiKey=c8c4a5a8779b4c4ca7407b2cc855ea89`
+    );
+    const data = await response.json();
+
+    setPopular(data.recipes);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+      <div className="container mx-auto py-8 sm:px-12 md:px-24 h-full">
+        <h1 className="text-4xl font-serif font-bold mb-10">Popular picks today</h1>
+        
+        <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
+        {popular.map((recipe) => {
+        return (
+          <div
+            className="flex flex-col justify-between card border-2 rounded-lg  p-2 mx-auto"
+            key={recipe.id}
+          >
+            <img
+              className="aspect-video object-cover rounded-md mb-2"
+              src={recipe.image}
+              alt=""
+            />
+            <div className="flex justify-between mb-2">
+              <h1 className="text-xl font-semibold w-64 truncate ...">{recipe.title}</h1>
+              <h1 className="text-xl font-semibold">$4</h1>
+            </div>
+            <button className="py-2 px-4 bg-violet-500 hover:bg-violet-700 w-full rounded-md text-white font-semibold text-xl">
+              Add to Cart
+            </button>
+          </div>
+        );
+      })}
+        </div>
+      
+    </div>
+  );
+}
+
+export default Popular;
