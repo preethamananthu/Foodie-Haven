0c1b4e55cdcbaf31131a9db0e2d001b2422702e3
preethamananthu <preethamananthu@gmail.com>
2023-07-28 20:25:00 +0530

    ham menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import { FaHamburger } from "react-icons/fa";
 import { RxCross1 } from "react-icons/rx";
 
 function Header() {
-const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
   return (
     <div className="container mx-auto py-4 sm:px-12 md:px-24 flex justify-between text-2xl items-center border-b-2">
@@ -17,22 +17,15 @@ const [open, setOpen] = useState(false);
       </nav>
 
       <div className="lg:hidden">
-        <button className="relative"
-        onClick={()=> {setOpen(!open)
-          console.log(open);
-         }}
+        <button
+          className="relative"
+          onClick={() => {
+            setOpen(!open);
+          }}
         >
-          <FaHamburger />
+          {open ? <RxCross1 /> : <FaHamburger />}
         </button>
-        
-          
         {open && <Navlinks />}
-        
-        <button className="absolute top-[6vh] right-[6vh] lg:hidden text-neutral-800 text-4xl z-10" 
-        onClick={()=>{
-          setOpen(!open)
-          console.log(open)
-        }}><RxCross1 /></button>
       </div>
       <ul className="hidden lg:flex gap-8 text-neutral-500">
         <li>
--- src/components/Navlinks.tsx
+++ src/components/Navlinks.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react"
-
-import { RxCross1 } from "react-icons/rx";
-
 function Navlinks() {
-    const [open, setOpen] = useState(false);
   return (
     <div>
         <div className="
-        absolute w-full h-full top-0 left-0 bg-slate-200 bg-opacity-[98%]
+        absolute w-full h-full top-[65px] left-0 bg-slate-200 bg-opacity-[98%]
         lg:static lg:w-fit lg:bg-opacity-0
         ">
         <ul className="
@@ -15,15 +10,6 @@ function Navlinks() {
             gap-20 text-neutral-800 relative
             lg:flex-row lg:font-normal lg:h-full lg:text-neutral-500 lg:text-2xl
         ">
-
-
-        <button className="absolute top-[6vh] right-[6vh] lg:hidden text-neutral-800 text-4xl z-10" 
-        onClick={()=>{
-          setOpen(!open)
-          console.log(open)
-        }}><RxCross1 /></button>
-        
-
           <li>
             <a href="" className="hover:text-neutral-800">
               Home
